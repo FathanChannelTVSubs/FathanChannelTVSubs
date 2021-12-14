8fb303d953c68e972d7b77e285ee029fd0c4d38e
FathanChannelTVSubs <96095677+FathanChannelTVSubs@users.noreply.github.com>
2021-12-14 15:43:23 +0700

    FNF IMPOSTER WEEKS SHEET.
    fuck off.

---
+++ source/Weeks.hx
@@ -0,0 +1,146 @@
+{
+	"songs": [
+		[
+			"Thunderstorm",
+			"ssussytomongus",
+			[
+				246,
+				153,
+				135
+			]
+		],
+		[
+			"God-Eater",
+			"SsussyTomongus",
+			[
+				246,
+				153,
+				135
+			]
+		],
+		[
+			"Talladega",
+			"ssussytomongus",
+			[
+				246,
+				153,
+				135
+			]
+		]
+	],
+	"hideFreeplay": true,
+	"weekBackground": "stage",
+	"weekCharacters": [
+		"sus",
+		"bf",
+		"gf"
+	],
+	"storyName": "When The iMpostor Is SUs :)",
+	"weekName": "Bakka",
+	"freeplayColor": [
+		146,
+		113,
+		253
+	],
+	"hideStoryMode": false,
+	"weekBefore": "tutorial",
+	"startUnlocked": true
+}{
+	"songs": [
+		[
+			"Watch-Out",
+			"greenysus",
+			[
+				66,
+				255,
+				93
+			]
+		],
+		[
+			"Apprehensive",
+			"greenysus",
+			[
+				66,
+				255,
+				93
+			]
+		],
+		[
+			"Double-Trouble",
+			"greenysus",
+			[
+				66,
+				255,
+				93
+			]
+		]
+	],
+	"hideFreeplay": false,
+	"weekBackground": "stage",
+	"weekCharacters": [
+		"green",
+		"bf",
+		"gf"
+	],
+	"storyName": "Stop Posting About Among Us! I'm Tired Of It!",
+	"weekName": "Shit",
+	"freeplayColor": [
+		146,
+		113,
+		253
+	],
+	"hideStoryMode": false,
+	"weekBefore": "tutorial",
+	"startUnlocked": true
+}
+{
+	"songs": [
+		[
+			"Sussus-Toogus",
+			"shapeshifter",
+			[
+				0,
+				255,
+				255
+			]
+		],
+		[
+			"Lights-Down",
+			"shapeshifter",
+			[
+				146,
+				113,
+				253
+			]
+		],
+		[
+			"Reactor",
+			"shapeshifter",
+			[
+				146,
+				255,
+				133
+			]
+		]
+	],
+	"hideFreeplay": false,
+	"weekBackground": "stage",
+	"difficulties": "",
+	"weekCharacters": [
+		"shape",
+		"bf",
+		"gf"
+	],
+	"storyName": "Da New Role Amogus Role! But On FNF...",
+	"weekName": "SHIT.gg",
+	"freeplayColor": [
+		146,
+		113,
+		253
+	],
+	"hideStoryMode": false,
+	"weekBefore": "tutorial",
+	"startUnlocked": true
+}
+
+
