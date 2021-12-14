c0ab1e6f1993a0cdbc50810505d960172fe24762
FathanChannelTVSubs <96095677+FathanChannelTVSubs@users.noreply.github.com>
2021-12-14 09:43:47 +0700

    Your Profile On The Game
    OH SHIT BRUH.

---
+++ Profile.hx
@@ -0,0 +1,12 @@
+{
+  name: Unknown
+   description: The new player
+     bio: My real name is Fathan LOL
+       
+       setName: "Unknown"
+         setDescription: "The new player
+          setBio: "My real name is Fathan LOL"
+           PlayerNumber: "#0000"
+             PlayerColor: ["120, 34, 09"]
+               PlayerPicture: "shit.png"
+}               
