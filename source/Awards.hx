11622e5225ff2c9d67527c5f46a2feeb204da0aa
FathanChannelTVSubs <96095677+FathanChannelTVSubs@users.noreply.github.com>
2021-12-14 10:34:24 +0700

    Awards State On Psych Engine
    LOLLY POP.

--- source/Awards.hx
+++ source/Awards.hx
@@ -5,13 +5,28 @@
       awardsHowtoget: "Complete A Week Without Idle."
         
  awardsList: "2nd"
-   awardsName: "Engineer Go Brr"
-     awardsDescription: "Engineer Can Vent. But, Boyfriend Just Eject Him."
-       awardsHowtoget: "Complete Engineer Week On Hard."
+  awardsName: "Engineer Go Brr"
+    awardsDescription: "Engineer Can Vent. But, Boyfriend Just Eject Him."
+      awardsHowtoget: "Complete Engineer Week On Hard."
  
  awardsList: "3rd"
-   awardsName: "Death Of Shapeshifter"
-     awardsDescription: "Noobest Shapeshifter. LOL."
-       awardsHowtoget: "Complete Shapeshifter Week On Hard
+  awardsName: "Death Of Shapeshifter"
+    awardsDescription: "Noobest Shapeshifter. LOL."
+      awardsHowtoget: "Complete Shapeshifter Week On Hard."
+        
+ awardsList: "4th"
+  awardsName: "Greeny Sus"
+    awardsDescription: "Green Is Gay Bruh."
+      awardsHowtoget: "Complete Green Imposter Week On Hard."
+       
+ awardsList "5th"
+  awardsName: "Red Is Sus"
+    awardsDescription: "Literally, Red Is Always Sus."
+      awardsHowtoget: "Complete Red Imposter Week On Hard."
+       
+ awardsList: "6th"
+  awardsName: "The Full Combo Master"
+    awardsDescription: "You Are An FNF Master!"
+      awardsHowtoget: "Complete All Songs With Full Combo."
 }
      
