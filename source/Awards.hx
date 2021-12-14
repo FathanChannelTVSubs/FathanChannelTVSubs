44ac84caa9a909953e250b65478d3c04c2fbcfe9
FathanChannelTVSubs <96095677+FathanChannelTVSubs@users.noreply.github.com>
2021-12-14 10:15:37 +0700

    Awards Psych Engine LOL.
    Nothing To See Here.

---
+++ source/Awards.hx
@@ -0,0 +1,17 @@
+{
+ awardsList: "1st" 
+  awardsName: "You Don't Have Idle"
+    awardsDescription: "Insanely Really You Does'nt Have An Idle."
+      awardsHowtoget: "Complete A Week Without Idle."
+        
+ awardsList: "2nd"
+   awardsName: "Engineer Go Brr"
+     awardsDescription: "Engineer Can Vent. But, Boyfriend Just Eject Him."
+       awardsHowtoget: "Complete Engineer Week On Hard."
+ 
+ awardsList: "3rd"
+   awardsName: "Death Of Shapeshifter"
+     awardsDescription: "Noobest Shapeshifter. LOL."
+       awardsHowtoget: "Complete Shapeshifter Week On Hard
+}
+     
