38a4d8e24ea4734840c052fcaecba920edeaad94
FathanChannelTVSubs <96095677+FathanChannelTVSubs@users.noreply.github.com>
2021-12-15 08:05:43 +0700

    Main Menu Things
    With Code

---
+++ source/MainMenu.hx
@@ -0,0 +1,26 @@
+package
+
+imp. haxeflixel
+ api. shitass
+   fathan.com/api
+     ass.gffatsex
+       
+{
+ [timerstarts]
+  "startOn:5seconds"
+    show: "seizure.png"
+      wait - "5seconds"
+        show: "storymodebutton", "youtubebutton", "awardsbutton", "etc".
+          showFolder: "mainmenu"
+           showBG: "AmongUsSubscribe", "AmongUsStory", "etc".
+         seizure=true
+             button=true
+               shit=false
+          noNeedLUA=true
+}
+{
+ else if
+   shitpngshow=false
+   other=true
+     cryaboutitshow=false       
+}
