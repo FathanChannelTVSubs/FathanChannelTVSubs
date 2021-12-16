75d137d814437afe332999b08d2e3a4d1075b80f
FathanChannelTVSubs <96095677+FathanChannelTVSubs@users.noreply.github.com>
2021-12-16 17:26:59 +0700

    Is It Worth?
    YUP!

---
+++ source/SongChartSave.hx
@@ -0,0 +1,33 @@
+import flx.flixel 
+import newgrounds.flixel
+import haxe.flixel
+import fathan.flixel
+import luajit.flixel
+import gx.flixel
+import shit.flixel
+
+{
+ // event = startOn = shit
+  // event = stopOn = done
+   
+  startEvent["startOn"] = '4Seconds'
+    eventShitStarts=true
+      eventFuckedUp=false
+        stopEvent["stopOn" = '5Seconds'
+               shitTest=true
+                  fuckTest=false
+                startSheet=true
+                  stopSheet=false
+             chartSave("true", "false")
+          shitAss=true
+       startSaveAt["Sus Timer"]
+    suscribe=false
+      
+   // imports.gg.flixel
+      // imports=true
+       //annoy=false
+      timer=true
+    // shitass
+  // fuck
+ // lolshit=true
+}
